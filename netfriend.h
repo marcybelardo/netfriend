6466a44d3b43bfa0b286756e3237797adbc79b92
Marceline Belardo <marcelinebelardo@gmail.com>
2025-07-28 21:58:58 +0300

    streamlined implementation

--- netfriend.h
+++ netfriend.h
@@ -16,20 +16,15 @@
 
 #define NF_VERSION 1
 
-#define NF_BACKLOG 8
-
-enum {
-	NF_UNIX = 0,
-	NF_INET = 1,
-};
+#define NF_BACKLOG 16
 
 // Creates listening socket. Takes hostname, port, and socket family enum
 // Returns file descriptor of socket, or -1 on error
-int nf_listen_conn(const char *, const char *, int);
+int nf_listen_conn(const char *host, const char *port);
 
 // Accepts new connection. Takes server file descriptor, and socket family enum
 // Returns file descriptor of socket, or -1 on error
-int nf_accept_conn(int, int);
+int nf_accept_conn(int fd);
 
 #endif // NETFRIEND_H
 
@@ -53,90 +48,62 @@ static int nf_set_nonblock(int fd)
 
 	flags = fcntl(fd, F_GETFL, 0);
 	if (flags == -1) {
-		perror("set_nonblock");
+		perror("nf_set_nonblock fcntl F_GETFL");
 		return -1;
 	}
 	res = fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 	if (res == -1) {
-		perror("set_nonblock");
+		perror("nf_set_nonblock fcntl F_SETFL");
 		return -1;
 	}
 
 	return 0;
 }
 
-static int nf_set_tcpnodelay(int fd)
+static int nf_bind_conn(const char *host, const char *port)
 {
-	return setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &(int){1}, sizeof(int));
-}
-
-static int nf_bind_conn_unix(const char *sockp)
-{
-	struct sockaddr_un addr;
-	int fd;
-
-	if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-		perror("bind_conn_unix socket()");
-		return -1;
-	}
-
-	memset(&addr, 0, sizeof(addr));
-	addr.sun_family = AF_UNIX;
-	strncpy(addr.sun_path, sockp, sizeof(addr.sun_path) - 1);
-	unlink(sockp);
-	if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-		perror("bind_conn_unix bind()");
-		return -1;
-	}
-
-	return fd;
-}
-
-static int nf_bind_conn_tcp(const char *host, const char *port)
-{
+	struct addrinfo hints, *res, *rp;
+	int sfd;
+	int yes = 1;
+	
+	memset(&hints, 0, sizeof(hints));
 	struct addrinfo hints = {
-		.ai_family = AF_UNSPEC,
+		.ai_family = AF_INET,
 		.ai_socktype = SOCK_STREAM,
 		.ai_flags = AI_PASSIVE,
 	};
-	struct addrinfo *res, *rp;
-	int sfd;
 
 	if (getaddrinfo(host, port, &hints, &res) != 0) {
-		perror("bind_conn_tcp getaddrinfo()");
+		perror("nf_bind_conn() getaddrinfo");
 		return -1;
 	}
 
 	for (rp = res; rp; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-		if (sfd == -1)
+		if ((sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) == -1) {
+			perror("nf_bind_conn socket");
 			continue;
-		if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
-			perror("bind_conn_tcp setsockopt()");
-		if ((bind(sfd, rp->ai_addr, rp->ai_addrlen)) == 0)
-			break;
+		}
+		if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+			perror("nf_bind_conn setsockopt");
+			return -1;
+		}
+		if ((bind(sfd, rp->ai_addr, rp->ai_addrlen)) == 0) {
+			close(sfd);
+			perror("nf_bind_conn bind");
+			continue;
+		}
 
-		close(sfd);
+		break;
 	}
 
+	freeaddrinfo(res);
+	
 	if (!rp) {
-		perror("bind_conn_tcp no bind");
+		fprintf(stderr, "nf_bind_conn failed to bind\n");
 		return -1;
 	}
-	freeaddrinfo(res);
 
 	return sfd;
-}
-
-static int nf_bind_conn(const char *host, const char *port, int sock_family)
-{
-	int fd;
-	if (sock_family == NF_UNIX)
-		fd = nf_bind_conn_unix(host);
-	else
-		fd = nf_bind_conn_tcp(host, port);
-
-	return fd;
 }
 
 void *nf_getinaddr(struct sockaddr *sa)
@@ -148,38 +115,40 @@ void *nf_getinaddr(struct sockaddr *sa)
 }
 
 
-int nf_listen_conn(const char *host, const char *port, int sock_family)
+int nf_listen_conn(const char *host, const char *port)
 {
 	int fd;
 
-	if ((fd = nf_bind_conn(host, port, sock_family)) == -1)
+	if ((fd = nf_bind_conn(host, port)) == -1)
 		return -1;
-	if ((nf_set_nonblock(fd)) == -1)
+	if ((nf_set_nonblock(fd)) == -1) {
 		return -1;
-	if (sock_family == NF_INET)
-		nf_set_tcpnodelay(fd);
+	}
 	if ((listen(fd, NF_BACKLOG)) == -1) {
-		perror("listen_conn listen()");
+		perror("nf_listen_conn listen");
 		return -1;
 	}
 
 	return fd;
 }
 
-int nf_accept_conn(int fd, int sock_family)
+int nf_accept_conn(int fd)
 {
 	int clientfd;
-	struct sockaddr_in addr;
-	socklen_t addrlen = sizeof(addr);
-
-	if ((clientfd = accept(fd, (struct sockaddr *)&addr, &addrlen)) < 0)
-		return -1;
-	nf_set_nonblock(clientfd);
-
-	if (sock_family == NF_INET)
-		nf_set_tcpnodelay(clientfd);
-
+	struct sockaddr_storage client_addr;
+	socklen_t addrlen = sizeof(client_addr);
 	char ipbuf[INET_ADDRSTRLEN + 1];
+
+	if ((clientfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen)) < 0) {
+		perror("nf_accept_conn accept");
+		return -1;
+	}
+	
+	if ((nf_set_nonblock(clientfd)) == -1) {
+		close(clientfd);
+		return -1;
+	}
+
 	if (inet_ntop(AF_INET, &addr.sin_addr, ipbuf, sizeof(ipbuf)) == NULL) {
 		close(clientfd);
 		return -1;
